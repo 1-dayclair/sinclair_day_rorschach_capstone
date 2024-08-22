da3c14d2253f0e5474d78c9dc0f6d64feaab6ff9
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-08-22 13:56:11 -0400

    update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = 4000;
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://global-circular.onrender.com", 
+    origin: "https://interglobal-circular.onrender.com", 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
