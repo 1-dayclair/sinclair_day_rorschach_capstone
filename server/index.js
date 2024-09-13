da3450e928c687e4b10ceef584cd57092685551f
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-09-13 03:56:15 -0400

    update

--- server/index.js
+++ server/index.js
@@ -38,6 +38,13 @@ async function main() {
 main().catch(err => console.log(err));
 // Connection^^
 
+app.use((req, res, next) => {
+    if (req.path.endsWith(".html")) {
+        res.setHeader("Content-Type", "text/html");
+    }
+    next();
+});
+
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
