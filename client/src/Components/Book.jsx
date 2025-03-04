3e9d97fec02afd7c7f316a79a6b73b41d9f4bf99
super-time1 <168475235+super-time1@users.noreply.github.com>
2025-03-04 17:08:19 -0500

    under construction

--- client/src/Components/Book.jsx
+++ client/src/Components/Book.jsx
@@ -1,24 +1,28 @@
-import React from "react";
-import HTMLFlipBook from "react-pageflip"; 
-import "../styles/rorschach_v2.css";
+// import React from "react";
+// import HTMLFlipBook from "react-pageflip"; 
+// import "../styles/rorschach_v2.css";
 
-function WorldTalks() {
-    return (
-        <div className="magazine">
-            <HTMLFlipBook className="magazine" width="500" height="500">
-                    <div id="pg0"> <img src="../media/1_extracted_Letter Land! 1-10-images-0.jpg" width="500" height="500" alt="Page 0 of World Talks! Issue #1" /></div>
-                    <div id="pg1"> <img src="../media/1_extracted_Letter Land! 1-10-images-1.jpg" width="500" height="500" alt="Page 1"></img></div>
-                    <div id="pg2"> <img src="../media/1_extracted_Letter Land! 1-10-images-2.jpg" width="500" height="500" alt="Page 2"></img></div>
-                    <div id="pg3"> <img src="../media/1_extracted_Letter Land! 1-10-images-3.jpg" width="500" height="500" alt="Page 3"></img></div>
-                    <div id="pg4"> <img src="../media/1_extracted_Letter Land! 1-10-images-4.jpg" width="500" height="500" alt="Page 4"></img></div>
-                    <div id="pg5"> <img src="../media/1_extracted_Letter Land! 1-10-images-5.jpg" width="500" height="500" alt="Page 5"></img></div>
-                    <div id="pg6"> <img src="../media/1_extracted_Letter Land! 1-10-images-6.jpg" width="500" height="500" alt="Page 6"></img></div>
-                    <div id="pg7"> <img src="../media/1_extracted_Letter Land! 1-10-images-7.jpg" width="500" height="500" alt="Page 7"></img></div>
-                    <div id="pg8"> <img src="../media/1_extracted_Letter Land! 1-10-images-8.jpg" width="500" height="500" alt="Page 8"></img></div>
-                    <div id="pg9"> <img src="../media/1_extracted_Letter Land! 1-10-images-9.jpg" width="500" height="500" alt="Page 9"></img></div>        
-            </HTMLFlipBook>
-        </div>
-    );
-}
+// function WorldTalks() {
+//     return (
+//         <div className="magazine">
+//             <HTMLFlipBook className="magazine1" width="500" height="500">
+//                     <div id="pg0"> <img src="../media/1_extracted_Letter Land! 1-10-images-0.jpg" width="500" height="500" alt="Page 0 of World Talks! Issue #1" /></div>
+//                     <div id="pg1"> <img src="../media/1_extracted_Letter Land! 1-10-images-1.jpg" width="500" height="500" alt="Page 1"></img></div>
+//                     <div id="pg2"> <img src="../media/1_extracted_Letter Land! 1-10-images-2.jpg" width="500" height="500" alt="Page 2"></img></div>
+//                     <div id="pg3"> <img src="../media/1_extracted_Letter Land! 1-10-images-3.jpg" width="500" height="500" alt="Page 3"></img></div>
+//                     <div id="pg4"> <img src="../media/1_extracted_Letter Land! 1-10-images-4.jpg" width="500" height="500" alt="Page 4"></img></div>
+//                     <div id="pg5"> <img src="../media/1_extracted_Letter Land! 1-10-images-5.jpg" width="500" height="500" alt="Page 5"></img></div>
+//                     <div id="pg6"> <img src="../media/1_extracted_Letter Land! 1-10-images-6.jpg" width="500" height="500" alt="Page 6"></img></div>
+//                     <div id="pg7"> <img src="../media/1_extracted_Letter Land! 1-10-images-7.jpg" width="500" height="500" alt="Page 7"></img></div>
+//                     <div id="pg8"> <img src="../media/1_extracted_Letter Land! 1-10-images-8.jpg" width="500" height="500" alt="Page 8"></img></div>
+//                     <div id="pg9"> <img src="../media/1_extracted_Letter Land! 1-10-images-9.jpg" width="500" height="500" alt="Page 9"></img></div>        
+//             </HTMLFlipBook>
+//             {/* <HTMLFlipBook className="magazine2" width="500" height="500">
+//                 <div> </div>
 
-export default WorldTalks;  
+//             </HTMLFlipBook> */}
+//         </div>
+//     );
+// }
+
+// export default WorldTalks;  
